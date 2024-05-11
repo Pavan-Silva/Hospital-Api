f3daf5524cc86b7440b815f9cc895923edcfc1ad
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2024-05-11 19:13:12 +0530

    build - implemented crud for doctor

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const Doctor = require("./models/doctor.model");
 
-app.get("/test", function (req, res) {
+const app = express();
+app.use(express.json());
+
+app.get("/test", (req, res) => {
   res.send("API is running");
+});
+
+app.get("/api/doctors", async (req, res) => {
+  try {
+    const doctors = await Doctor.find({});
+    res.json(doctors);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/doctors/:id", async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id);
+    res.json(doctor);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/doctors", async (req, res) => {
+  try {
+    const doctor = await Doctor.create(req.body);
+    res.status(201).json(doctor);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/api/doctors/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const doctor = await Doctor.findByIdAndUpdate(id, req.body);
+
+    if (!doctor) {
+      return res.status(404).json({ message: "Doctor not found" });
+    }
+
+    const updatedDoctor = await Doctor.findById(id);
+    res.status(200).json(updatedDoctor);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/api/doctors/:id", async (req, res) => {
+  try {
+    const doctor = await Doctor.findByIdAndDelete(req.params.id);
+
+    if (!doctor) {
+      return res.status(404).json({ message: "Doctor not found" });
+    }
+
+    res.status(200).json({ message: "Doctor deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
---
+++ models/doctor.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const DoctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+    },
+
+    gender: {
+      type: String,
+      required: [true, "Gender is required"],
+    },
+
+    specialization: {
+      type: String,
+      required: false,
+    },
+
+    current_appointments: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+
+  { timestamps: true }
+);
+
+const Doctor = mongoose.model("Doctor", DoctorSchema);
+module.exports = Doctor;
