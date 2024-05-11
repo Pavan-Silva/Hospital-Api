a354ea5168ab40d77ebbb69154fd147269daabb4
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2024-05-11 19:36:56 +0530

    build - implemented a cleaner file architecture

---
+++ controllers/doctor.controller.js
@@ -0,0 +1,66 @@
+const Doctor = require("../models/doctor.model");
+
+const getDoctors = async (req, res) => {
+  try {
+    const doctors = await Doctor.find({});
+    res.json(doctors);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getDoctorById = async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id);
+    res.json(doctor);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createDoctor = async (req, res) => {
+  try {
+    const doctor = await Doctor.create(req.body);
+    res.status(201).json(doctor);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const doctor = await Doctor.findByIdAndUpdate(id, req.body);
+
+    if (!doctor) {
+      return res.status(404).json({ message: "Doctor not found" });
+    }
+
+    const updatedDoctor = await Doctor.findById(id);
+    res.status(200).json(updatedDoctor);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteDoctor = async (req, res) => {
+  try {
+    const doctor = await Doctor.findByIdAndDelete(req.params.id);
+
+    if (!doctor) {
+      return res.status(404).json({ message: "Doctor not found" });
+    }
+
+    res.status(200).json({ message: "Doctor deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getDoctors,
+  getDoctorById,
+  createDoctor,
+  updateDoctor,
+  deleteDoctor,
+};
---
+++ routes/doctor.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getDoctors,
+  getDoctorById,
+  createDoctor,
+  updateDoctor,
+  deleteDoctor,
+} = require("../controllers/doctor.controller");
+
+router.get("/", getDoctors);
+router.get("/:id", getDoctorById);
+router.post("/", createDoctor);
+router.put("/:id", updateDoctor);
+router.delete("/:id", deleteDoctor);
+
+module.exports = router;
